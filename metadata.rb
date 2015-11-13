f05e20ba04daeed2fd7ebb9f845c4f77acc6238e
Morgan Nelson <mnelson@steele-forge.com>
2015-11-13 15:09:42 -0600

    add name attribute
    to make spiceweasel happy(er)

--- metadata.rb
+++ metadata.rb
@@ -4,3 +4,4 @@ license          "MIT"
 description      "Installs/Configures tcpwatch-httpproxy"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
+name             "tcpwatch-httpproxy"
